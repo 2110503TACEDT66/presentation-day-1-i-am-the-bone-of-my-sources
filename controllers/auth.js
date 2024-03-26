c3e43badc8d3ebb361424b07fdfc3233c4c8c235
Taihenc <51960471+Taihenc@users.noreply.github.com>
2024-03-26 11:27:46 +0700

    typo

--- controllers/auth.js
+++ controllers/auth.js
@@ -19,7 +19,7 @@ exports.register = async (req, res, next) => {
     sendTokenResponse(user, 200, res);
   } catch (err) {
     console.log(err.stack);
-    res.status(400).json({ success: false }, { message: err.message });
+    res.status(400).json({ success: false, message: err.message });
   }
 };
 
